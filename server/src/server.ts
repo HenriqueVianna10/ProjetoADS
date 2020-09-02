1f20863f68686170b8d9c2369a954bf37ad081b8
Henrique Vianna <henriquev79@gmail.com>
2020-09-01 23:06:06 -0300

    adicionado biblioteca react

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,17 @@
 import express from 'express';
 
-const = express
+const app = express();
 
-app.
+app.get('/users', (request, response) => {
+    console.log('Listagem de Usuários');
+
+    //JSON
+
+    response.json([
+        'Diego',
+        'Cleiton',
+        'Robson'
+    ]);
+});
+
+app.listen(3333);
