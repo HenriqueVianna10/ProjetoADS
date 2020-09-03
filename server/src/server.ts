2d2bf700bc64b86071eca6f81a3b9a22c8418aa5
Henrique Vianna <henriquev79@gmail.com>
2020-09-02 23:03:34 -0300

    inicio do back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,40 @@
 import express from 'express';
 
 const app = express();
+// Rotas: endereço
+// Recurso: Entidade do Sistema
+
+// GET: Busca
+// POST: Criar
+// PUT: Atualizar
+// DELETE: Deletar
+
+// POST http://localhost:3333/users = criar usuário
+// GET  http://localhost:3333/users = listar usuários
+// GET http://localhost:3333/users/5 = Buscar Dados do usuário com ID 5
+
+const users = [
+    
+    'Diego',
+    'Cleiton',
+    'Robson'
+];
 
 app.get('/users', (request, response) => {
     console.log('Listagem de Usuários');
 
-    //JSON
+   return response.json(users);
+});
 
-    response.json([
-        'Diego',
-        'Cleiton',
-        'Robson'
-    ]);
+app.get('/users/:id')
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Henrique',
+        email: 'henriquev79@gmail.com.br'
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
