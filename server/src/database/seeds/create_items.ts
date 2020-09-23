3bfee7553768aa2754859e61dabc759f1f36908d
Henrique Vianna <henriquev79@gmail.com>
2020-09-22 22:57:42 -0300

    back-end

---
+++ server/src/database/seeds/create_items.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex';
+
+export async function seed(knex: Knex) {
+   await knex('items').insert([
+       {title: 'Rotavírus Humano', image: 'vacina_logo.svg'},
+       {title: 'Tetravalente Viral', image: 'vacina_logo.svg'},
+       {title: 'Poliomielite Viral', image: 'vacina_logo.svg'},
+       {title: 'Meningite A e C', image: 'vacina_logo.svg'},
+       {title: 'Febre Amarela', image: 'vacina_logo.svg'},
+       {title: 'Tríplice Viral', image: 'vacina_logo.svg'},
+    ]);
+}
