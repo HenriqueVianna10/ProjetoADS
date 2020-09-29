ded0a1faa43a35179d47aeccdb0b4c6eaa1bca42
Henrique Vianna <henriquev79@gmail.com>
2020-09-28 23:22:39 -0300

    inicio do front-end

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import logo from '../../assets/logo.svg';
+
+
+const Home = () => {
+    return (
+       <div id="page-home">
+           <div className="content">
+               <img src={logo} alt="Evacina" />
+           </div>
+       </div>
+    )
+}
+
+export default Home;
