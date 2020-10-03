a35b3d137c82e2cfbdc67035f96fc19f3b89c684
Henrique Vianna <henriquev79@gmail.com>
2020-10-02 21:57:55 -0300

    Inserção do mapa e finalizaçãodo formulário

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {FiArrowLeft} from 'react-icons/fi';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker} from 'react-leaflet';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/sus-logo.png';
 
+interface  Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response =>{
+            setItems(response.data);
+        });
+    },[]);
+
     return (
         <div id="page-create-point">
             <header>
@@ -61,7 +78,31 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa </span>
                     </legend>
 
-                    
+                        <Map center={[-30.1015063,-51.2587587]} zoom={15}>
+                        <TileLayer 
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-30.1015063,-51.2587587]}/>
+
+                        </Map>
+
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado(UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma uf</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma Cidade</option>
+                            </select>
+                        </div>
+                    </div>
                 </fieldset>
 
                 <fieldset>
@@ -69,8 +110,20 @@ const CreatePoint = () => {
                         <h2>Vacinas Disponíveis</h2>
                         <span>Selecione uma ou mais vacinas</span>
                     </legend>
+
+                    <ul className="items-grid">
+                        {items.map(item =>(
+                        <li>
+                            <img src={item.image_url} alt={item.title}/>
+                            <span>{item.title}</span>
+                        </li>
+                        ))}
+                    </ul>                  
                 </fieldset>
 
+                <button type="submit">
+                    Cadastrar Ponto de Saúde
+                </button>
             </form>
         </div>
     );
